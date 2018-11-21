54ddcb4bd4fd6d31a2af2c66becbcfadf3177600
aleksandar.svorcan <tehnomaniac@gmail.com>
2018-11-21 17:16:06 +0100

    File system mocks fixed.

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,7 +1,7 @@
-from unittest.mock import patch
+from unittest import mock
+from PIL import Image
 from django.test import TestCase
 from django.contrib.auth.models import User
-from PIL import Image
 
 
 class ProfileTest(TestCase):
@@ -9,15 +9,17 @@ class ProfileTest(TestCase):
     def setUp(self):
         user = User.objects.create_superuser(username='admin', email='admin@bogusemail.com', password='admin')
         self.profile = user.profile
+        self.new_image = Image.new('RGB', (600, 600), 'white')
 
     def test_str(self):
         self.assertEqual(str(self.profile), f'{self.profile.user.username} Profile')
 
-    @patch('Image.open')
-    def image_open_mock(self):
-        return Image.new('RGB', (600, 600), 'white')
-
-    def test_save(self):
+    @mock.patch('PIL.Image.open')
+    def test_save(self, mock_image_open):
+        mock_image_open.return_value = self.new_image
+        mock_image_open.return_value.save = lambda *args, **kwargs: None
         self.profile.save()
-        self.assertEqual(self.profile.image.height, 300)
-        self.assertEqual(self.profile.image.width, 300)
+        self.assertEqual(self.new_image.height, 300)
+        self.assertEqual(self.new_image.width, 300)
+        # self.assertTrue(mock_image.save.called)
+
